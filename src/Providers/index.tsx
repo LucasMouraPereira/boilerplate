e45a54aa1ff89e5b9236565b63591fe14656ab0a
Lucas <lucas@MacBook-Air-de-Lucas.local>
2023-11-30 21:44:02 -0300

    feat: start project

---
+++ src/Providers/index.tsx
@@ -0,0 +1,18 @@
+'use client'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyles } from 'src/utils/styles/GlobalStyles'
+import { theme } from 'src/utils/styles/theme'
+import { Container } from 'src/components/ui/Container'
+
+import type { ChildrenProps } from 'src/utils/types/global.types'
+
+export const Providers = ({ children }: ChildrenProps) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Container className={`${theme.roboto_mono} font-sans`}>
+        {children}
+      </Container>
+    </ThemeProvider>
+  )
+}
